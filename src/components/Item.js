05c78f45a98f3c9aa39a4a86738d47f14c8d38ee
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-19 21:35:47 +0300

    Applied the adding/Removing from cart styling buttons

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [ChangeText, setChangeText] = useState(true);
+  function addToRemoveFromCart() {
+    setChangeText(!ChangeText);
+  }
+  const btnText = ChangeText ? "Add to Cart" : "Remove From Cart";
+  const liClassName = ChangeText ? "" : "in-cart";
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToRemoveFromCart}>
+        {btnText}
+      </button>
     </li>
   );
 }
